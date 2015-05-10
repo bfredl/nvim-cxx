e01eafed8e596634b9b14fd2a24bac2502a2a38d
Björn Linse <bjorn.linse@gmail.com>
2015-05-10 17:30:39 +0200

    Sketch async event handling

--- src/test.cpp
+++ src/test.cpp
@@ -3,6 +3,7 @@
 #include <sstream>
 #include <msgpack.hpp>
 #include <boost/asio.hpp>
+#include <boost/bind.hpp>
 
 using std::cerr;
 using std::endl;
@@ -15,43 +16,61 @@ enum {
     NOTIFY = 2
 };
 
-class NvimClient {
+template<typename response_handler, typename event_handler> class AsyncNvimClient {
     asio::io_service io;
     // TODO: virtualize the socket, to allow multible connection types
     stream_protocol::socket s{io};
     msgpack::unpacker up;
     uint64_t sid = 0;
+    std::unordered_map<uint64_t, response_handler> waiting;
+    bool running = false;
 
-    // synchronous test
-    bool next_message(msgpack::unpacked& result) {
-        if (up.next(result)) {
-            return true;
-        }
-        size_t avail;
-        while(avail = s.available()) {
-            up.reserve_buffer(std::min(32*1024ul,avail));
-            boost::system::error_code error;
-            size_t read = s.read_some(asio::buffer(up.buffer(), up.buffer_capacity()), error);
-            up.buffer_consumed(read);
-            if (up.next(result)) {
-                return true;
-            }
-        }
-        return false;
+    void read_more() {
+        up.reserve_buffer(32*1024ul);
+        s.async_read_some(asio::buffer(up.buffer(), up.buffer_capacity()),
+                boost::bind(&AsyncNvimClient::handle_read, this, _1, _2));
     }
 
+    void handle_read(const boost::system::error_code& error, size_t read) {
+        if (error) {
+            exit(1);
+        }
+        msgpack::unpacked result;
+        up.buffer_consumed(read);
+        while (up.next(result)) {
+            handle_message(result);
+        }
+        if (running) read_more();
+    }
+
+    void handle_message(msgpack::unpacked &result) {
+        cerr << result.get() << endl;
+        running = false;
+    }
 
 public:
-    bool connect(const std::string address) {
-        s.connect(stream_protocol::endpoint(address));
+    template<typename... Args> void request_async(std::string name, std::tuple<Args...> args) {
         std::stringstream sb;
-        std::tuple<int, int, std::string, std::tuple<>> t(REQUEST, sid++, "vim_get_api_info", {});
+        std::tuple<int, int, std::string, std::tuple<Args...>> t(REQUEST, sid++, name, args);
         msgpack::pack(sb,t);
         asio::write(s, asio::buffer(sb.str()));
-        msgpack::unpacked result;
-        while(!next_message(result)) {} //sync test
-        cerr << result.get() << endl;
     }
+
+
+    bool connect(const std::string address) {
+        s.connect(stream_protocol::endpoint(address));
+    }
+
+    void run() {
+        running = true;
+        read_more();
+        io.run();
+    }
+
+};
+
+// placeholder
+class NvimClient : public AsyncNvimClient<int,int> {
 };
 
 int main() {
@@ -59,9 +78,10 @@ int main() {
     char* addr = getenv("NVIM_LISTEN_ADDRESS");
     if(!addr) return 0;
     nv.connect(addr);
+    nv.request_async("vim_get_api_info", {});
+    nv.run();
 }
 
 /*
-noremap <Plug>ch:un :up<cr><c-w><c-w>:terminal make run<cr><c-\><c-n>:set bufhidden=wipe<cr><c-w><c-w>
 noremap <Plug>ch:un :up<cr><c-w><c-w>imake run<cr><c-\><c-n><c-w><c-w>
 */
